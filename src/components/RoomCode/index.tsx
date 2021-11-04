ded9593d168623b65b60e94e4d56d1976bc94407
Marcelo Haubrih <marcelohaubrih@gmail.com>
2021-11-04 11:46:22 -0300

    Projeto Letmeask

---
+++ src/components/RoomCode/index.tsx
@@ -0,0 +1,24 @@
+import toast from 'react-hot-toast';
+import copyImg from '../../assets/images/copy.svg';
+
+import './styles.scss';
+
+type RoomCodeProps = {
+  code: string;
+}
+
+export function RoomCode(props: RoomCodeProps) {
+  function copyRoomCodeToClipboard(){
+    navigator.clipboard.writeText(props.code);
+    toast.success('Codigo da sala copiado!');
+  }
+
+  return (
+    <button className="room-code" onClick={copyRoomCodeToClipboard}>
+      <div>
+        <img src={copyImg} alt="Copy room code" />
+      </div>
+      <span>Sala #{props.code}</span>
+    </button>
+  )
+}
